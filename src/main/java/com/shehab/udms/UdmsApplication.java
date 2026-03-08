d9f38b8beffd234b484a50491d03e2a53624243f
shehab097 <shehabsarder97@gmail.com>
2026-03-08 14:49:32 +0600

    first commit

---
+++ src/main/java/com/shehab/udms/UdmsApplication.java
@@ -0,0 +1,13 @@
+package com.shehab.udms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shehab/udms/UdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shehab.udms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
