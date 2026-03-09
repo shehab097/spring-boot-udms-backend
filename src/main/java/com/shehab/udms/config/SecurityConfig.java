6853e4fc1b340291de214d060b789c907fe83745
shehab097 <shehabsarder97@gmail.com>
2026-03-10 01:15:31 +0600

    login password check and setup for jwt token generate

--- src/main/java/com/shehab/udms/config/SecurityConfig.java
+++ src/main/java/com/shehab/udms/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.shehab.udms.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,7 +32,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("/register","/login").permitAll()
+                                .anyRequest().authenticated()
+                )
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
@@ -40,10 +47,16 @@ public class SecurityConfig {
     public DaoAuthenticationProvider authenticationProvider() {
         // 1. Use the no-argument constructor
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-
         provider.setPasswordEncoder(new BCryptPasswordEncoder(10));
-
-
         return provider;
+    }
+
+
+
+    // create bean for authenticationManager
+    // AuthenticationManager is an interface, we cant create object of it
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager(); // will talk with auth manager
     }
 }
--- src/main/java/com/shehab/udms/service/UserService.java
+++ src/main/java/com/shehab/udms/service/UserService.java
@@ -3,6 +3,9 @@ package com.shehab.udms.service;
 import com.shehab.udms.model.Users;
 import com.shehab.udms.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,22 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    AuthenticationManager authManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10); // we can pass straight
 
     public Users register(Users user){
         // before sending data to the database, we can encrypt password
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
+    }
+
+    public String verify(Users user) {
+        // verify
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated())
+            return "success";
+        return "failed";
     }
 }
