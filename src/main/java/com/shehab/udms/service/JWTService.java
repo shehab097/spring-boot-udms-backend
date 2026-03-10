9f1eaa7ac8c88e3f656988db0a0fdf2a47a444e7
shehab097 <shehabsarder97@gmail.com>
2026-03-10 23:26:06 +0600

    JWT token validation done

--- src/main/java/com/shehab/udms/service/JWTService.java
+++ src/main/java/com/shehab/udms/service/JWTService.java
@@ -72,7 +72,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 min
                 .and()
                 .signWith(getKey())
                 .compact();
