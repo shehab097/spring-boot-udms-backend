f6574f0864871ef25242cf566c2d3b1ebff3b6ef
shehab <150367787+shehab097@users.noreply.github.com>
2026-04-02 01:52:04 +0600

    update Test.java

--- src/main/java/com/shehab/udms/controller/Test.java
+++ src/main/java/com/shehab/udms/controller/Test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class Test {
 
     @GetMapping("/")
     public String sayHello(HttpServletRequest httpServletRequest){
